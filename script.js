71e3dc3af7f5978c1f8c9ea1a95f32072454e305
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-07-21 10:19:33 -0700

    Added playRound function

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 function getComputerChoice() {
     /** Generate a number between 1 and 3 */
     let computerChoice = Math.floor(Math.random() * 3) + 1;
-    console.log(computerChoice);
 
     /** Initialize returnedChoice string */
     let returnedChoice = '';
 
     /** Convert random number to relevant string */
     if (computerChoice === 1) {
-        returnedChoice = 'Rock';
+        returnedChoice = 'rock';
     } else if (computerChoice === 2) {
-        returnedChoice = 'Paper';
+        returnedChoice = 'paper';
     } else {
-        returnedChoice = 'Scissors'
+        returnedChoice = 'scissors'
     }
-
-    console.log(returnedChoice);
     /** Return the randomly chosen string */
     return returnedChoice;
 }
@@ -24,7 +21,28 @@ function getComputerChoice() {
 let playerSelection = prompt("Rock, Paper, or Scissors?");
 /** Convert player input string to lowercase for standardization */
 playerSelection = playerSelection.toLowerCase();
-console.log(playerSelection);
 
 /** Store computer selection in a variable */
 let computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    let outcomeString = '';
+
+    if (playerSelection == computerSelection) {
+        outcomeString = "It's a tie! Play again.";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        outcomeString = "You lose! Paper beats Rock.";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        outcomeString = "You win! Rock beats Scissors.";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        outcomeString = "You win! Paper beats Rock.";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        outcomeString = "You lose! Scissors beats Paper.";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        outcomeString = "You lose! Rock beats Scissors.";
+    } else {
+        outcomeString = "You win! Scissors beats Paper.";
+    }
+
+    return outcomeString;
+}
