97ab8fd1b6297a7c84abb0b59228be28f75fdbe1
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-07-21 09:56:33 -0700

    Added returnedChoice string w/RPS names

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function getComputerChoice() {
     /** Generate a number between 1 and 3 */
     let computerChoice = Math.floor(Math.random() * 3) + 1;
     console.log(computerChoice);
+
+    let returnedChoice = '';
+
+    if (computerChoice === 1) {
+        returnedChoice = 'Rock';
+    } else if (computerChoice === 2) {
+        returnedChoice = 'Paper';
+    } else {
+        returnedChoice = 'Scissors'
+    }
 }
 
 getComputerChoice();
