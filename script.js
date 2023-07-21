1d3524f641395d52187b8e3244e08a666c2bcbf9
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-07-21 09:52:04 -0700

    Added console.log to test for computerChoice

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getComputerChoice() {
     /** Generate a number between 1 and 3 */
     let computerChoice = Math.floor(Math.random() * 3) + 1;
+    console.log(computerChoice);
 }
+
+getComputerChoice();
