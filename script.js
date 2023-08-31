ecd28907e009e947d041c94a98249fc658dd509d
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-08-30 18:44:11 -0700

    Added constants for button DOM elements

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/** Instantiate variables for button elements */
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
 function getComputerChoice() {
     /** Generate a number between 1 and 3 */
     let computerChoice = Math.floor(Math.random() * 3) + 1;
