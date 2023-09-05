9e1d51160ef98b4c856e861acd8025a6a73028b5
lucaslija <81848948+lucaslija@users.noreply.github.com>
2023-09-04 18:21:16 -0700

    Buttons disabled after win

--- newScript.js
+++ newScript.js
@@ -30,6 +30,12 @@ function getComputerChoice() {
     return returnedChoice;
 }
 
+function disableButtons() {
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+}
+
 /** Play a single round of Rock, Paper, Scissors */
 function playRound(playerSelection) {
     let outcomeString = '';
@@ -38,20 +44,36 @@ function playRound(playerSelection) {
     if (playerSelection == computerSelection) {
         outcomeString = "It's a tie! Play again.";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++;
         outcomeString = "You lose! Paper beats Rock.";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++;
         outcomeString = "You win! Rock beats Scissors.";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
         outcomeString = "You win! Paper beats Rock.";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
         outcomeString = "You lose! Scissors beats Paper.";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++;
         outcomeString = "You lose! Rock beats Scissors.";
     } else {
+        playerScore++;
         outcomeString = "You win! Scissors beats Paper.";
     }
 
     outcomeP.innerHTML = "<h1>" + outcomeString + "</h1>";
+    pScoreSpan.textContent = playerScore;
+    cpuScoreSpan.textContent = computerScore;
+
+    if (playerScore === 5) {
+        outcomeP.innerHTML = "<h1>You reached five points and won the game!<h1>";
+        disableButtons();
+    } else if (computerScore === 5) {
+        outcomeP.innerHTML = "<h1>The computer reached five points and won the game!<h1>";
+        disableButtons();
+    }
 }
 
 rockBtn.addEventListener('click', () => {
